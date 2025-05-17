fb7c09ca5622e72dac4a3637b8b45d48bee7100f
Mouad <mouro016@uottawa.ca>
2025-05-17 19:04:24 -0400

    chore: Gradle skeleton (Windows)

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.cyclonedx.bom") version "1.8.1"
+}
+allprojects {
+    group = "io.aegisq"
+    version = "0.1.0-SNAPSHOT"
+    repositories { mavenCentral() }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+﻿rootProject.name = "aegisq-ca"
+include("libs:common-model",
+        "services:auth-server",
+        "services:ca-service",
+        "services:ocsp-service",
+        "services:audit-service",
+        "services:gateway")
